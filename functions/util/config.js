341cb4c983b5e2012f4b1787428c34deca36ef1f
OHMOOYOUNG <moh@csumb.edu>
2020-01-29 16:16:31 -0800

    first commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBqqHIvwhcQ85yTfi0AQoRPmiZyZmzxrGU",
+  authDomain: "truckee-f9191.firebaseapp.com",
+  databaseURL: "https://truckee-f9191.firebaseio.com",
+  projectId: "truckee-f9191",
+  storageBucket: "truckee-f9191.appspot.com",
+  messagingSenderId: "813529661083",
+  appId: "1:813529661083:web:a0f0e9409faa7723e2d03e",
+  measurementId: "G-K58GVXD35G"
+};
